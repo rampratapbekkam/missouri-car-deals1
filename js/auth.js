6fe6b350e6e473ce009dcd34a340e625f4fff686
rampratapbekkam <rampratap.bekkam@slu.edu>
2026-01-20 12:15:30 -0600

    adding login and sign up

---
+++ js/auth.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  document.getElementById("authModal").style.display = "flex";
+};
+
+// Temporary login/signup actions
+document.querySelector(".login-btn").addEventListener("click", () => {
+  alert("Login clicked (backend will be added later)");
+});
+
+document.querySelector(".signup-btn").addEventListener("click", () => {
+  alert("Signup clicked (backend will be added later)");
+});
